d0dabcd31264850f681a85f0419b1b9023e39938
Edgar Pino <edgarbeltran@me.com>
2020-04-09 21:01:35 -0600

    Clean up code

--- cluster-measures/main.py
+++ cluster-measures/main.py
@@ -4,44 +4,52 @@ import math
 
 def cluster_probability(cluster):
     cluster_sum = []
-    n = float(len(cluster))
+    cluster_len = float(len(cluster))
     cluster_names = list(set(cluster))
+
     for index, cluster_name in enumerate(cluster_names):
         cluster_sum.append(0)
         for i in cluster:
             if cluster_name == i:
                 cluster_sum[index] += 1
-    cluster_prob = [i / n for i in cluster_sum]
-    return cluster_prob
+
+    return [i / cluster_len for i in cluster_sum]
 
 
 def entropy(cluster):
-    cluster_prob = cluster_probability(cluster)
     entropy = 0
+    cluster_prob = cluster_probability(cluster)
+
     for i in cluster_prob:
         entropy += i * math.log(i, 2)
+
     return entropy * (-1)
 
 
 def mutual_information(cluster, truth):
-    n = float(len(cluster))
-    pc = cluster_probability(cluster)
-    pt = cluster_probability(truth)
+    cluster_len = float(len(cluster))
+    sc = list(set(cluster))
+    st = list(set(truth))
+    mutual_info = 0
+    c_probability = cluster_probability(cluster)
+    truth_probability = cluster_probability(truth)
 
-    sc = list(set(cluster))  # name of the clusters
-    st = list(set(truth))  # name of the clusters in ground truth
-
-    # declare empty array to store p_ij
-    p_matrix = [[0 for x in range(len(sc))] for y in range(len(st))]
+    probability_matrix = [[0 for x in range(len(sc))] for y in range(len(st))]
 
     for i in range(len(truth)):
-        p_matrix[st.index(truth[i])][sc.index(cluster[i])] += 1
-    mutual_info = 0
+        probability_matrix[st.index(truth[i])][sc.index(cluster[i])] += 1
+
     for i in range(len(st)):
         for j in range(len(sc)):
-            joint_p = p_matrix[i][j] / (n)
-            if joint_p != 0:
-                mutual_info += joint_p * (math.log(joint_p / float(pc[j] * pt[i]), 2))
+            joint_probability = probability_matrix[i][j] / (cluster_len)
+            if joint_probability != 0:
+                mutual_info += joint_probability * (
+                    math.log(
+                        joint_probability
+                        / float(c_probability[j] * truth_probability[i]),
+                        2,
+                    )
+                )
     return mutual_info
 
 
